f505053d56960f809f2f05ed50466a4bbcc815cf
hjk <j20173237@seps9.deu.ac.kr>
2019-12-09 14:59:33 +0900

    seat

---
+++ seat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SEAT 10
+
+void newline(){
+	printf("\n");
+}
+
+main() {
+
+	int i;
+
+	printf("----------------------------------------------");
+	newline();
+	printf("|       ||       ||       ||       ||       |");
+	newline();
+	printf("|   1   ||   2   ||   3   ||   4   ||   5   |");
+	newline();
+	printf("|       ||       ||       ||       ||       |");
+	newline();
+	printf(" -------  -------  -------  -------  ------- ");
+
+	newline();
+	newline();
+	newline();
+	newline();
+
+	printf(" -------  -------  -------  -------  ------- ");
+	newline();
+	printf("|       ||       ||       ||       ||       |");
+	newline();
+	printf("|   6   ||   7   ||   8   ||   9   ||   10  |");
+	newline();
+	printf("|       ||       ||       ||       ||       |");
+	newline();
+	printf("----------------------------------------------");
+	newline();
+} 
